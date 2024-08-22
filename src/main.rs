7b9654ce93b723c0a7bff16837bdd83c04c6db65
cheesecake <oweaibnxoweaib@gmail.com>
2024-08-21 20:03:08 -0700

    Works now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use hex::decode;
+use rand::prelude::*;
 use std::io;
 use std::io::Write;
+use std::str;
 use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
@@ -15,12 +18,13 @@ fn main() {
                 Ok(1) => {
                     print!("Input text to encrypt: ");
                     io::stdout().flush().unwrap();
-                    
+
                     let mut text = String::new();
                     let _ = io::stdin().read_line(&mut text);
 
                     print!("Input encryption length from 0 to 255: ");
                     io::stdout().flush().unwrap();
+
 
                     'key_loop: loop {
                         let mut key = String::new();
@@ -28,7 +32,7 @@ fn main() {
 
                         match key.trim().parse::<u8>() {
                             Ok(k) => {
-                                println!("{}", enc(&text.trim(), k));
+                                println!("Encrypted text: {}", enc(&text.trim(), k));
 
                                 break 'key_loop;
                             }
@@ -42,13 +46,29 @@ fn main() {
                     break 'inner;
                 }
                 Ok(2) => {
-                    print!("Input string to encrypt: ");
+                    print!("Input string to decrypt: ");
                     io::stdout().flush().unwrap();
 
-                    let mut str = String::new();
-                    let _ = io::stdin().read_line(&mut str);
 
-                    println!("{}", dec(&str.trim()));
+                    'dec_loop: loop {
+                        let mut str = String::new();
+                        let _ = io::stdin().read_line(&mut str);
+                        if str.trim().eq("C") || str.trim().eq("c") {
+                            break 'dec_loop;
+                        }
+
+                        match dec(&str.trim()) {
+                            Ok(result) => {
+                                print!("Decrypted text: {result}");
+
+                                break 'dec_loop;
+                            }
+                            _ => {
+                                print!("Invalid string, input C to cancel or input a valid string to continue: ");
+                                io::stdout().flush().unwrap();
+                            }
+                        }
+                    }
 
                     break 'inner;
                 }
@@ -68,22 +88,48 @@ fn main() {
 
 fn enc(input: &str, key: u8) -> String {
     let mut out = format!("{form:0>2}", form = format!("{key:x}")).to_string();
+    let mut rng = thread_rng();
 
-    for n in 0..(input.graphemes(true).count() - 1) {
-        let mut hex = String::new();
-        let _ = input;
-        match input.trim().chars().nth(n).parse::<u8>() {
-            Ok(h) => {
-                hex = format!("{h:x}");
-            }
-            _ => println!(":(")
+    let mut i = 0;
+    for _n in input.graphemes(true) {
+        let hex = format!("{:x}", input.trim().chars().nth(i).unwrap() as u8);
+        i += 1;
+
+        out = out + &*hex.graphemes(true).nth(0).unwrap().to_string();
+
+        for _n in 0..key {
+            out = out + &format!("{:x}", rng.gen_range(0..=15))
         }
-        out = out + hex
+
+        out = out + &*hex.chars().nth(1).unwrap().to_string();
+
+        for _n in 0..key {
+            out = out + &format!("{:x}", rng.gen_range(0..=15))
+        }
     }
-    
+
     out
 }
 
-fn dec(input: &str) -> String {
-    format!("Input: {input}").to_owned()
+fn dec(input: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let key = (i32::from_str_radix(&*(input.graphemes(true).nth(0).unwrap_or("").to_string()
+        + input.graphemes(true).nth(1).unwrap_or("")), 16)?) + 1;
+
+
+    let mut out = String::new();
+
+    let mut pos: i32 = 2;
+
+    let realchars = (input.graphemes(true).count() - 2) as i32 / (key * 2);
+
+    for _n in 0..realchars {
+        let mut hex = input.graphemes(true).nth(pos as usize).unwrap().to_owned();
+        pos += key;
+        hex += input.graphemes(true).nth(pos as usize).unwrap();
+        pos += key;
+        out += str::from_utf8(&*decode(hex).unwrap())?;
+    }
+
+    Ok(out)
 }
+
