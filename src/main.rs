9fde402feb026d21b2eba7e73814103ef1ebdfdb
cheesecake <oweaibnxoweaib@gmail.com>
2024-08-21 23:51:01 -0700

    Added a rough cli argument mode

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,99 @@
 use hex::decode;
 use rand::prelude::*;
-use std::io;
-use std::io::Write;
-use std::str;
+use std::{env, fs, fs::File, io, io::Write, process, str, error::Error};
 use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        let config = Config::build(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        });
+
+        println!("Path: {}\nOperation: {}\nKey: {}", config.in_path, config.op, config.key);
+
+        if let Err(e) = quiet(config) {
+            println!("Application error: {e}");
+            process::exit(1);
+        }
+    } else {
+        cli_interface();
+    }
+}
+
+struct Config {
+    in_path: String,
+    op: String,
+    key: String,
+    out_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 4 {
+            return Err("not enough arguments");
+        }
+
+        let in_path = args[1].clone();
+        let op = args[2].clone();
+        let key = args[3].clone();
+        let out_path = args[4].clone();
+
+        Ok(Config { in_path, op, key, out_path})
+    }
+}
+
+fn quiet(config: Config) -> Result<String, Box<dyn Error>> {
+    let text = fs::read_to_string(config.in_path)?;
+
+    if config.op == "Encrypt" {
+        match config.key.trim().parse::<u8>() {
+            Ok(k) => {
+                let encrypted = enc(&text.trim(), k);
+                if config.out_path.starts_with("_") {
+                    println!("Encrypted text: {}", encrypted);
+                } else {
+                    let mut file = File::create(config.out_path)?;
+                    file.write_all(encrypted.as_bytes())?;
+                }
+                process::exit(0);
+            }
+            _ => {
+                println!("Invalid key");
+                process::exit(1);
+            }
+        }
+    } else if config.op == "Decrypt" {
+        match dec(&text.trim()) {
+            Ok(result) => {
+                if config.out_path.starts_with("_") {
+                    println!("Decrypted text: {result}");
+                } else {
+                    let mut file = File::create(config.out_path)?;
+                    file.write_all(result.as_bytes())?;
+                }
+                process::exit(0);
+            }
+            _ => {
+                print!("Invalid input file");
+                process::exit(1);
+            }
+        }
+    } else {
+        println!("No valid operation selected");
+        process::exit(1);
+    }
+}
+
+fn cli_interface() {
     'outer: loop {
         print!("Input 1 for encryption, 2 for decryption, or 3 to exit: ");
         io::stdout().flush().unwrap();
 
         'inner: loop {
             let mut input = String::new();
-            let _ = io::stdin().read_line(&mut input).expect("0");
+            let _ = io::stdin().read_line(&mut input);
 
             match input.trim().parse::<u8>() {
                 Ok(1) => {
@@ -59,7 +140,7 @@ fn main() {
 
                         match dec(&str.trim()) {
                             Ok(result) => {
-                                print!("Decrypted text: {result}");
+                                println!("Decrypted text: {result}");
 
                                 break 'dec_loop;
                             }
@@ -85,7 +166,6 @@ fn main() {
         }
     }
 }
-
 fn enc(input: &str, key: u8) -> String {
     let mut out = format!("{form:0>2}", form = format!("{key:x}")).to_string();
     let mut rng = thread_rng();
@@ -114,7 +194,6 @@ fn enc(input: &str, key: u8) -> String {
 fn dec(input: &str) -> Result<String, Box<dyn std::error::Error>> {
     let key = (i32::from_str_radix(&*(input.graphemes(true).nth(0).unwrap_or("").to_string()
         + input.graphemes(true).nth(1).unwrap_or("")), 16)?) + 1;
-
 
     let mut out = String::new();
 
